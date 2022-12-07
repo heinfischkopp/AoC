07aa83bdd282ffaf19d733683edf7bad89cc38ca
heinfischkopp <119623014+heinfischkopp@users.noreply.github.com>
2022-12-07 11:05:41 +0100

    2022 Day 7

--- 2022/day6.py
+++ 2022/day6.py
@@ -2,16 +2,13 @@ import MyTools
 
 puzzle = MyTools.AoC(6,1)
 
-
 for line in puzzle.readLines():
-    dic=[]
-    dic_n=[]
-    solution_a=0
-    solution_b=0
+    solution_a=4
+    solution_b=14
 
-    while len(list(set(sorted(line[solution_a:solution_a+4])))) !=4:
+    while len(list(set(sorted(line[solution_a-4:solution_a])))) !=4:
         solution_a+=1
-    while len(list(set(sorted(line[solution_b:solution_b+14])))) !=14:
+    while len(list(set(sorted(line[solution_b-14:solution_b])))) !=14:
         solution_b+=1
-    puzzle.solution(solution_a+4)
-    puzzle.solution(solution_b+14)
+    puzzle.solution(solution_a)
+    puzzle.solution(solution_b)
---
+++ 2022/day7.py
@@ -0,0 +1,69 @@
+import MyTools
+
+puzzle = MyTools.AoC(7)
+
+class myfile:
+    def __init__(self,name,parentdir):
+        self.name=name
+        self.size=None
+        self.parentdir=parentdir
+        self.children={}
+        self.files={}
+
+    def addFile(self,name,size):
+        self.files[name]=int(size)
+    
+    def addDir(self,name):
+        self.children[name]=myfile(name,self)
+    
+    def changeDir(self,newDir):
+        if newDir == "..":
+            return self.parentdir
+        else:
+            return self.children[newDir]
+
+    def getSize(self):
+        s = sum(self.files.values())
+        for d in self.children.values():
+            s+=d.getSize()
+        self.size=s
+        return s
+
+    def puzzleA(self):
+        s=0
+        for d in self.children.values():
+            s+=d.puzzleA()
+        if self.getSize() < 100000:
+            return self.size + s
+        else:
+            return s
+
+    def puzzleB(self,freespaceneeded):
+        s = [float("inf")]
+        if self.size > freespaceneeded:
+            s.append(self.size)
+        for d in self.children.values():
+            s.append(d.puzzleB(freespaceneeded))
+        return min(s)
+
+root=myfile("/",None)
+currentDir=root
+lines=puzzle.readLines()
+i = 1
+while i < len(lines): 
+    line = lines[i].split()
+    match line[1]:
+        case "ls":
+            while i+1<len(lines) and lines[i+1].split()[0] != "$":
+                i+=1
+                line = lines[i].split()
+                if line[0]=="dir":
+                    currentDir.addDir(line[1])
+                else:
+                    currentDir.addFile(line[1],line[0])
+        case "cd":
+            currentDir = currentDir.changeDir(line[2])
+    i+=1
+
+puzzle.solution(root.puzzleA())
+puzzle.solution(root.puzzleB(30000000-(70000000-root.getSize())))
